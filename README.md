007c96f43742f3943a1d397721a0660c967cb57d
micahcourey <micahjcourey@gmail.com>
2016-01-13 21:29:09 -0800

    Add initial project files

---
+++ README.md
@@ -0,0 +1,10 @@
+React Github Profile Viewer
+===========================
+
+By Micah Courey, 13-Jan-2016
+
+A React component that uses the Github API to display users profile info.
+
+Technologies Used
+----------
+JavaScript, React, jQuery, AJAX, HTML, CSS
---
+++ script.jsx
@@ -0,0 +1,33 @@
+var Card = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+  componentDidMount: function() {
+    var component = this;
+    $.get("https://api.github.com/users/" + this.props.login, function(data) {
+      component.setState(data);
+    });
+  }, 
+  render: function() {
+    return (
+      <div>
+        <img src={this.state.avatar_url} width="100" />
+        <h3>{this.state.name}</h3>
+        <hr/>
+      </div>
+    );
+  }
+});
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Card login="micahcourey" />
+        <Card login="petehunt" />
+      </div>
+    )
+  }
+});
+
+React.render(<Main />, document.getElementById("root"));
